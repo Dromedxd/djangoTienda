6578938e91ad78c54f2c3289ff8d78de9a9ae76d
Dromedxd <101174062+Dromedxd@users.noreply.github.com>
2023-06-28 15:58:42 +0200

    Info about products done

--- pythonProject1/tiendaOnline/paginaCompras/models.py
+++ pythonProject1/tiendaOnline/paginaCompras/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    categoria = models.TextField(default='Sin categoria')
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/')
+    disponible = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class ProductoHome(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.producto.nombre
+
+
+class Cliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    correo_electronico = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=20)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.user.username
--- pythonProject1/tiendaOnline/paginaCompras/views.py
+++ pythonProject1/tiendaOnline/paginaCompras/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Producto
+
+def home(request):
+    productos = Producto.objects.all()
+    context = {
+        'productos': productos
+    }
+    return render(request, 'Paginacompras/home.html', context)
 
 
-def index(request):
-    return HttpResponse("Tu primera vista")
+def detail(request, producto_id):
+    producto = Producto.objects.get(id=producto_id)
+    context = {
+        'producto': producto
+    }
+    return render(request, 'Paginacompras/detail.html', context)
